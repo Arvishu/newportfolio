6189bfc09997b7561b2f0475c3e0c5bbc4d0252d
Hugo Arvis <arvishugo@hotmail.fr>
2020-09-08 15:04:07 +0200

    change design section

--- src/components/Design/Design.js
+++ src/components/Design/Design.js
@@ -1,15 +1,37 @@
 import React, { useState } from 'react';
+import jaguar1 from './../../ressources/jaguar1.svg';
+import jaguar2 from './../../ressources/jaguar2.svg';
+import jaguar3 from './../../ressources/jaguar3.svg';
+import jaguar4 from './../../ressources/jaguar4.svg';
 
 import './Design.css';
 
 export default function Design() {
 
 
-    return (
-       <div className='designContainer'>
-            <div className='designStyle'>
-                Design component
+
+    const jaguarDesigns = [
+        { id: 1, import: jaguar1, title: 'jaguar1', content: 'jaguar agressif' },
+        { id: 2, import: jaguar2, title: 'jaguar2', content: 'jaguar inquiet' },
+        { id: 3, import: jaguar3, title: 'jaguar3', content: 'jaguar curieux' },
+        { id: 4, import: jaguar4, title: 'jaguar4', content: 'jaguar pensif' }
+    ]
+
+    const listJaguarDesigns = jaguarDesigns.map((jaguarDesign) =>
+        <div >
+            <div  >
+
+                <img className="imgStyle" heigh="128px" width="128px" key={jaguarDesign.id} src={jaguarDesign.import} />
+                <h3>{jaguarDesign.content}</h3>
             </div>
-       </div >
+        </div>
+    );
+
+    return (
+        <div className="designContainer" >
+
+            <div className="designStyle">{listJaguarDesigns}</div>
+
+        </div >
     )
 }
--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -3,28 +3,29 @@ import './Project.css'
 
 
 export default function Project(props) {
-    
-   const{image,title,link}=props
-    
+
+    const { image, title, link } = props
+
     return (
         <div className="flip-card">
             <div className="flip-card-inner">
                 <div className="flip-card-front"></div>
                 <div className="flip-card-front">
-                    <img src={image}alt={image} style={{width:'25vw',height:'25vw'}} />
+                    <img src={image} alt={image} style={{ width: '25vw', height: '25vw' }} />
                     <h1>{title}</h1>
-    </div>
-                    <div className="flip-card-back">
-                        <h1>{title}</h1>
-                        <p>Architect & Engineer</p>
-                        <div>
-                        <a href={link}target="_blank">
-                        <img alt={image} src={image}
-                            width="48" height="48"
-                            title={"Venez visitez "+title+". Cliquez sur l'icone"}/></a>
-                        </div>
+                </div>
+                <div className="flip-card-back">
+                    <h1>{title}</h1>
+                    <p>Architect & Engineer</p>
+                    <div  >
+                        <a  href={link} target="_blank">
+                            <img className ={"imgPosition","button-glow"} 
+                            alt={image} src={image}
+                                width="64" height="64"
+                                title={"Venez visitez " + title + ". Cliquez sur l'icone"} /></a>
                     </div>
                 </div>
             </div>
+        </div>
     )
 }
