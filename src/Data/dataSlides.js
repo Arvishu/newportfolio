2ebaeeea1c1382cd2674ee4114783ec12cc4133e
Hugo Arvis <arvishugo@hotmail.fr>
2020-09-15 17:10:03 +0200

    add some pictures

--- src/Data/dataSlides.js
+++ src/Data/dataSlides.js
@@ -2,16 +2,26 @@ export default [
     {
         title: 'First slide!',
         subtitle: 'is it working?',
-        image: 'https://picsum.photos/300',
+        image: 'https://picsum.photos/id/1041/1285/685',
     },
     {
         title: "Second Slide",
         subtitle: "ohhh yeah",
-        image: "https://picsum.photos/400"
+        image: "https://picsum.photos/id/532/1286/685"
     },
     {
         title: "Third Slide",
+        subtitle: "another one! ✨",
+        image: "https://picsum.photos/id/6/1287/685"
+    },
+    {
+        title: "Fourth Slide",
+        subtitle: "another one! ✨",
+        image: "https://picsum.photos/id/60/1288/685"
+    },
+    {
+        title: "Fifth Slide",
         subtitle: "last one! ✨",
-        image: "https://picsum.photos/350"
+        image: "https://picsum.photos/1289/685"
     },
 ]
